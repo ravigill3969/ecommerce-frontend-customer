c694c8a546c0b4bfb227a7070410676fb887f87a
ravigill3936 <gill1234ravi@gmail.com>
2026-09-24 12:58:05 -0400

    fixed login page issue

--- src/App.tsx
+++ src/App.tsx
@@ -11,49 +11,28 @@ import Wishlist from "./page/Wishlist";
 import PaidOrders from "./page/PaidOrders";
 import NewArrivals from "./page/NewArrivals";
 import  About from "./page/About";
-
 function App() {
-  const { isAuthenticated } = useUser();
-  return (
-    <>
-    
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+  const { isAuthenticated, isLoading } = useUser();
 
-        <Route path="/" element={isAuthenticated ? <Home /> : <Login />} />
-        <Route path="/cart" element={isAuthenticated ? <Cart /> : <Login />} />
-        <Route
-          path="/profile"
-          element={isAuthenticated ? <Profile /> : <Login />}
-        />
-        <Route
-          path="/search"
-          element={isAuthenticated ? <Search /> : <Login />}
-        />
-        <Route
-          path="/success"
-          element={isAuthenticated ? <Success /> : <Login />}
-        />
-        <Route
-          path="/wishlist"
-          element={isAuthenticated ? <Wishlist /> : <Login />}
-        />
-        <Route
-          path="/orders"
-          element={isAuthenticated ? <PaidOrders /> : <Login />}
-        />
-        <Route
-          path="/new-arrivals"
-          element={isAuthenticated ? <NewArrivals /> : <Login />}
-        />
-        <Route
-          path="/about"
-          element={isAuthenticated ? <About />: <Login />}
-        />
-      </Routes>
-    </>
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/" element={isAuthenticated ? <Home /> : <Login />} />
+      <Route path="/cart" element={isAuthenticated ? <Cart /> : <Login />} />
+      <Route path="/profile" element={isAuthenticated ? <Profile /> : <Login />} />
+      <Route path="/search" element={isAuthenticated ? <Search /> : <Login />} />
+      <Route path="/success" element={isAuthenticated ? <Success /> : <Login />} />
+      <Route path="/wishlist" element={isAuthenticated ? <Wishlist /> : <Login />} />
+      <Route path="/orders" element={isAuthenticated ? <PaidOrders /> : <Login />} />
+      <Route path="/new-arrivals" element={isAuthenticated ? <NewArrivals /> : <Login />} />
+      <Route path="/about" element={isAuthenticated ? <About /> : <Login />} />
+    </Routes>
   );
 }
-
 export default App;
